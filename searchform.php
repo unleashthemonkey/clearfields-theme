3e339d7ada282ab1cc52bfe3f8e53cbbe337c507
Cristopher <cristopher@unleashthemonkey.nl>
2012-09-05 15:21:22 +0200

    added conditional for twitter ing

--- searchform.php
+++ searchform.php
@@ -5,3 +5,17 @@
 <input type="text" size="20" name="s" id="s" value="Write your search and hit Enter" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
 <input type="submit" id="searchsubmit" value="zoek"/>
 </form>
+<div id="twithome">
+<a href="http://twitter.com/clearfields">
+<img alt="twitter" src="<?php bloginfo('template_url'); ?>/images/<?php twitpic(); ?>" style="height:18px; width:18px;">
+</a>
+<?php
+function twitpic(){
+	if ( is_front_page() ) {
+		echo "twit.png";
+	} else {
+		echo "twit-black.png";
+	}
+}
+?>
+</div>
