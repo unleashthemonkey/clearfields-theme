de45326f5585239c9c44c608c36f93dd6b429e0a
Cristopher <cristopher@unleashthemonkey.nl>
2012-09-05 14:36:45 +0200

    added login link to basecamp

--- searchform.php
+++ searchform.php
@@ -1,5 +1,7 @@
-<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
-<div><input type="text" size="20" name="s" id="s" value="Write your search and hit Enter" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
-<input type="submit" id="searchsubmit" value="zoek"/>
+<div id="login">
+<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Basecamp' ) ) ); ?>">login</a>
 </div>
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<input type="text" size="20" name="s" id="s" value="Write your search and hit Enter" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
+<input type="submit" id="searchsubmit" value="zoek"/>
 </form>
