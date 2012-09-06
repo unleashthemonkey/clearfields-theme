4afd87a794cd4afb823534a44845c9c00f030ab4
Cristopher <cristopher@unleashthemonkey.nl>
2012-09-07 00:22:23 +0200

    load animation based on referrer

--- js/functions_page.js
+++ js/functions_page.js
@@ -1,7 +1,27 @@
 jQuery(document).ready(function ($) {
-	revealpage();
-
+	loadanimation();
 });
+
+function loadanimation(){
+	var refurl=document.referrer;
+	var referurlarray=refurl.split("/");
+	var thisurl=document.URL;
+	var thisurlarray=thisurl.split("/");
+	
+	if (referurlarray[3]==thisurlarray[3]){
+		revealpagenow();
+	}
+	else{
+		revealpage();
+	}
+}
+function revealpagenow()
+{
+	mostrarnow("#header-wrapper");
+	mostrarnow("#access");
+	mostrarnow("#colophon");
+	mostrarnow("#content");
+}
 
 function revealpage()
 {
--- js/page_animations.js
+++ js/page_animations.js
@@ -11,3 +11,17 @@ function mostrar(que,delay,tiempo) {
 	}
 	$(que).removeClass("invisible").hide().delay(delay).fadeIn(tiempo);
 }
+
+function mostrarslide(que,delay,tiempo) {
+	if(delay==undefined){
+		delay=500;
+	}
+	if(tiempo==undefined){
+		tiempo=1000;
+	}
+	$(que).removeClass("invisible").hide().delay(delay).slideDown(1000,'easeOutBack');
+}
+
+function mostrarnow(que){
+	$(que).removeClass("invisible").show();
+}
