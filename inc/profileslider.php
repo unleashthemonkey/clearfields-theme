3bf7fc2d255721fb1566d57fbd9f862e45f1f087
Cristopher <cristopher@unleashthemonkey.nl>
2012-09-18 12:37:43 +0200

    update to profileslider

--- inc/profileslider.php
+++ inc/profileslider.php
@@ -1,23 +1,35 @@
 <div id="slider-gallery">
 
-<div id="menu">
-
+<div id="menu" class="clearfix">
 <ul>
-	<li class="menuItem"><a href="">thumb 1</a></li>
-	<li class="menuItem"><a href="">thumb 2</a></li>
-	<li class="menuItem"><a href="">thumb 3</a></li>
-	<li class="menuItem"><a href="">thumb 4</a></li>
-	<li class="menuItem"><a href="">thumb 5</a></li>
+<?php $profile_query = new WP_Query(array('post_type' => 'profile','orderby' => 'menu_order', 'order' => 'ASC'));
+	if ($profile_query->have_posts()) {
+		while ($profile_query->have_posts()) : $profile_query->the_post();	?>
+			<li class="menuItem"><a href="">
+			<img src="<?php echo(types_render_field("profile-thumb-cf", array("raw"=>"true"))); ?>"
+			alt="<?php the_title(); ?>">
+			<h5><?php the_title(); ?></h5>
+			<h6><?php echo(types_render_field("job-title", array("raw"=>"true"))); ?></h6>
+			</a></li>
+		<?php endwhile; ?>
+	<?php } ?>	
 </ul>
 
 </div>
 
 <div id="slides">
-	<div class="slide"><h5>slide 1</h5></div>
-	<div class="slide"><h5>slide 2</h5></div>
-	<div class="slide"><h5>slide 3</h5></div>
-	<div class="slide"><h5>slide 4</h5></div>
-	<div class="slide"><h5>slide 5</h5></div>
+	<?php if ($profile_query->have_posts()) {
+		while ($profile_query->have_posts()) : $profile_query->the_post();	?>
+			<div class="slide clearfix" style="background: url(
+			<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+			$thumburl = $thumb['0'];			
+			echo($thumburl); ?>
+			) no-repeat;" alt="<?php the_title(); ?>">
+				<div class="profile-quote"><h4><?php the_content(); ?></h4></div>
+				<div class="namecaption"><h5><?php the_title(); ?></h5></div>
+			</div>
+		<?php endwhile; ?>
+	<?php } ?>
 </div>
 
 </div>
