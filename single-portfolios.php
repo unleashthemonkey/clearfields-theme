2652de2c0ae019870f45fd4007b06ed191df901e
Cristopher <cristopher@unleashthemonkey.nl>
2012-11-13 22:05:29 +0100

    added divs with overlays

--- single-portfolios.php
+++ single-portfolios.php
@@ -60,6 +60,35 @@ get_header('portfolios'); ?>
 					<?php endwhile; ?>
 					</div>
 					
+					<?php if ( $connected->have_posts() ) :
+					?>
+					<h3>Zij werkten er aan mee:</h3>
+					<div class="thumb-gallery clearfix">
+					<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
+					<div id="profile-<?php echo($slideCount); $slideCount++;?>" class="overlay" style="background-image:url(<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); $thumburl = $thumb['0']; echo($thumburl); ?>);">	
+					    <a class="close"></a>
+					    <?php if (is_page('Dit zijn wij')) : ?>
+					    <a href="<?php echo get_permalink(); ?>">
+					    <p><?php echo(types_render_field("profile-quote", array("raw"=>"true"))); ?></p>
+					    </a>
+					    <a href="<?php echo get_permalink(); ?>">
+					    <h6><?php the_title(); ?></h6>
+					    </a>
+					    <?php elseif (is_page('Alumni')): ?>
+					    <p><?php echo(types_render_field("profile-quote", array("raw"=>"true"))); ?></p>
+					    <h6><?php the_title(); ?><br>
+					    <span style="font-weight:normal;"><?php echo(types_render_field("job-title", array("raw"=>"true"))); ?></span></h6>
+					    <?php endif; ?>
+					    <div class="social">
+						    <p>
+						    <a href="<?php echo(types_render_field("profile-twitter", array("raw"=>"true"))); ?>" target="_blank"><img src="<?php bloginfo('template_url') ?>/images/twit.png" width="18" height="18" alt="Twitter"/></a>
+						    <a href="<?php echo(types_render_field("profile-linkedin", array("raw"=>"true"))); ?>" target="_blank"><img src="<?php bloginfo('template_url') ?>/images/linkedin.png" width="18" height="18" alt="LinkedIn"/></a>
+						    <p>
+					    </div>
+					</div>
+					<?php endwhile; ?>
+					</div>
+					
 					<?php 
 					// Prevent weirdness
 					wp_reset_postdata();
